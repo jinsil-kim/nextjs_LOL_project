66c45cc8a0a4532b383fab5a8b1ac8f336b562b3
jinsil <wlstlf9315@gmail.com>
2024-12-11 21:11:09 +0900

    feat: 디렉토리 설정 및 layout 링크 설정 완료

---
+++ src/app/champions/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ChapionsPage() {
+  return <div>ChapionsPage</div>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="py-14 text-3xl">
+          <nav className="flex justify-around">
+            <div>
+              <Link href={"/"}>Home</Link>
+            </div>
+            <div className="flex gap-16">
+              <Link href={"/champions"}>Champions</Link>
+              <Link href={"/items"}>Items</Link>
+              <Link href={"/rotation"}>Rotation</Link>
+            </div>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
